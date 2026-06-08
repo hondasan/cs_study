d356b97a80a8a41d6c901d2d2f48efd6ca51bfa1
honda <hondasan1981@gmail.com>
2026-06-08 11:50:41 +0900

    SEO対策（Canonical、OGP、JSON-LD、サイトマップ生成）および将来の収益化（Google Analytics、Google AdSense、広告プレースホルダー）の対応

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://hondasan.github.io',
   base: '/cs_study',
+  integrations: [sitemap()],
 });
